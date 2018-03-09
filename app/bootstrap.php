40874d0cdd227883da679c14d34d5113d3bdbaa5
dast <dast@seznam.cz>
2018-03-09 20:10:53 +0100

    some various edits/cleanups

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -11,8 +11,8 @@ $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
 $configurator->createRobotLoader()
-	->addDirectory(__DIR__. '/../vendor/dibi')
-	->addDirectory(__DIR__)
+	//->addDirectory(__DIR__. '/../vendor/dibi')
+	->addDirectory(APP_DIR)
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
--- www/index.php
+++ www/index.php
@@ -4,6 +4,20 @@
 // require __DIR__ . '/.maintenance.php';
 define('WWW_DIR', __DIR__);
 
+// absolute filesystem path to the application root
+define('APP_DIR', WWW_DIR . '/../app');
+
+// absolute filesystem path to the libraries
+define('LIBS_DIR', WWW_DIR . '/../libs');
+
+// absolute filesystem path to the temporary files
+define('TEMP_DIR', WWW_DIR . '/../temp');
+
+// absolute filesystem path to the log files
+define('LOG_DIR', WWW_DIR . '/../log/' . date('Y-m'));
+// Create log subdir
+if (!is_dir(LOG_DIR)) mkdir(LOG_DIR); 
+
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType('Nette\Application\Application')->run();
